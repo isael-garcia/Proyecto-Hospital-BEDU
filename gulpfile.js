507665a573906f6158e9551f2131090554ec781f
isael-garcia <69340389+isael-garcia@users.noreply.github.com>
2021-02-25 22:09:13 -0600

    agregando cards cuerpo principal

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css(){
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist : ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'expanded',
+        }))
+        .pipe(gulp.dest('css-gulp'));
+}
+    //tareas 
+    gulp.task('css-gulp', css);
+
+    function watchFiles(){
+        gulp.watch('scss/*.scss', css)
+        gulp.watch('*.html');
+    }
+    gulp.task('watch', gulp.parallel(watchFiles));
+
+const fileinclude = require('gulp-file-include');
+
+gulp.task('fileinclude', function() {
+  gulp.src(['index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./'));
+});
